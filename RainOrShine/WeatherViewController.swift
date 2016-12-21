948eedc98986e29230ce7ad151024f96f73ada28
Matt Deuschle <mdeuschle@gmail.com>
2016-12-21 15:36:17 -0800

    set up initial UI

---
+++ RainOrShine/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  RainOrShine
+//
+//  Created by Matt Deuschle on 12/21/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
